f46b2a79bd9b3dcfab4bcb64c4ee91c72d2ab679
takumibv <everybody.say.hey.yo@gmail.com>
2020-12-13 21:10:58 +0900

    画像追加

--- src/js/app.js
+++ src/js/app.js
@@ -1,3 +1,18 @@
-import '../scss/main.scss';
+import { gsap } from "gsap";
 
-console.log("main.js");
+console.log("gsap", gsap);
+
+// const tl = gsap.timeline({
+//   scrollTrigger: {
+//     trigger: "#hero",
+//     start: "top top",
+//     end: "bottom top",
+//     scrub: true
+//   }
+// });
+
+// gsap.utils.toArray(".js-parallax").forEach(layer => {
+//   const depth = layer.dataset.depth;
+//   const movement = -(layer.offsetHeight * depth)
+//   tl.to(layer, { y: movement, ease: "none" }, 0)
+// });
