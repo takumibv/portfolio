60b2d13f4dec9074bb11fb8a0ec6343447305306
takumibv <everybody.say.hey.yo@gmail.com>
2022-04-09 13:52:57 +0900

    next導入

---
+++ pages/index.tsx
@@ -0,0 +1,13 @@
+import type { NextPage } from "next";
+
+const Home: NextPage = () => {
+  return (
+    <div className="my-10">
+      <h1>
+        hello <span>Ta</span>
+      </h1>
+    </div>
+  );
+};
+
+export default Home;
